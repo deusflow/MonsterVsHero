bac3d77033dba858f4e39e9c116788dc52b64864
DeusFlow <deuswork@icloud.com>
2024-11-13 14:50:28 +0100

    Monster

--- Monster vc Heri/obj/Debug/net8.0/Monster vc Heri.AssemblyInfo.cs
+++ Monster vc Heri/obj/Debug/net8.0/Monster vc Heri.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Monster vc Heri")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f69c9b380976e27da54bbfd7e60e315ebead93a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Monster vc Heri")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Monster vc Heri")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
